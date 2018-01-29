31177501cc6e2ec6edc9ab79490d63270895f483
Chiara Wondany <cwondany@apollon.de>
2018-01-29 14:26:18 +0100

    images added

---
+++ e2e/imageComparison.ts
@@ -0,0 +1,13 @@
+
+
+/*var api = resemble(fileData).onComplete(function(data){
+  console.log(data);
+  /!*
+    {
+      red: 255,
+      green: 255,
+      blue: 255,
+      brightness: 255
+    }
+    *!/
+});*/
